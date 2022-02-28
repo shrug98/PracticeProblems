b39f0cb4c29fedd0893a585a799c1c787c0e54da
shrug98 <39962695+shrug98@users.noreply.github.com>
2022-02-27 23:58:51 -0500

    Update lastStoneWeight.java

--- lastStoneWeight.java
+++ lastStoneWeight.java
@@ -1,3 +1,10 @@
+//We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. 
+//Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+//If x == y, both stones are destroyed, and
+//If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+//At the end of the game, there is at most one stone left.
+//Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+
 import java.util.*;
 class Solution {
     public int lastStoneWeight(int[] stones) {
