df0ccac287b8b2b2660c0bcd02dd349800d5662b
shrug98 <39962695+shrug98@users.noreply.github.com>
2022-02-27 20:34:51 -0500

    Update maxProfit.java

--- maxProfit.java
+++ maxProfit.java
@@ -1,10 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
-        for (int i = 0; i < prices.length - 1; i++){
+        int max = 0; //variable for max profit
+        for (int i = 0; i < prices.length - 1; i++){  
             for(int j = i+1; j < prices.length; j++){
-                int profit = prices[j] - prices[i];
-                if (profit > max)
+                int profit = prices[j] - prices[i]; //checks the profit between days 
+                if (profit > max) //if this profit is greater than max, then profit is new max
                     max = profit;
             }
         }
