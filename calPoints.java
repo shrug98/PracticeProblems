d0f54d4ce3eb8db196bba93089e75566136970d1
shrug98 <39962695+shrug98@users.noreply.github.com>
2022-02-28 00:03:59 -0500

    Update calPoints.java

--- calPoints.java
+++ calPoints.java
@@ -1,3 +1,10 @@
+//At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record
+//An integer x - Record a new score of x.
+//"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+//"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+//"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+//Return the sum of all the scores on the record.
+
 import java.util.ArrayList;
 class Solution {
     public int calPoints(String[] ops) {
