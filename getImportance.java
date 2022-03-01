40427954f8dcf107fccada7fdb9d7802831f5f35
shrug98 <39962695+shrug98@users.noreply.github.com>
2022-02-28 22:09:19 -0500

    Update getImportance.java

--- getImportance.java
+++ getImportance.java
@@ -1,3 +1,14 @@
+//You have a data structure of employee information, including the employee's unique ID, importance value, and direct subordinates' IDs.
+
+//You are given an array of employees employees where:
+
+//    employees[i].id is the ID of the ith employee.
+//    employees[i].importance is the importance value of the ith employee.
+//    employees[i].subordinates is a list of the IDs of the direct subordinates of the ith employee.
+
+//Given an integer id that represents an employee's ID, return the total importance value of this employee and all their direct and indirect subordinates.
+
+
 /*
 // Definition for Employee.
 class Employee {
@@ -10,21 +21,19 @@ class Employee {
 class Solution {
     public int getImportance(List<Employee> employees, int id) {
         int total = 0;
+        int f = 0;
         for (int i = 0; i < employees.size(); i++){
             if(employees.get(i).id == id){
+                f = 1;
                 total += employees.get(i).importance;
                 if(!(employees.get(i).subordinates.isEmpty())){
                     for(int j = 0; j < (employees.get(i).subordinates).size(); j++){
-                        for(int k = 0; k < employees.size(); k++){
-                            if(employees.get(k).id == employees.get(i).subordinates.get(j)){
-                                total+=employees.get(k).importance;
-                                break;
+                        total += getImportance(employees, employees.get(i).subordinates.get(j));
                             } 
                     }
                 }
+            if (f == 1) break;
             }
-        }
-    }
         return total;
-}
+    }
 }
